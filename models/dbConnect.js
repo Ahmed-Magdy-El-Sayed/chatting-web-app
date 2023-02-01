2333fd55ee68202af1e618cffc8f831234f85203
Ahmed-Magdy-El-Sayed <109208478+Ahmed-Magdy-El-Sayed@users.noreply.github.com>
2023-02-01 18:20:34 +0200

    Update dbConnect.js

--- models/dbConnect.js
+++ models/dbConnect.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export default cb=>{
         return new Promise(async (resolve, reject)=>{
-                await mongoose.connect('mongodb+srv://AhmedMagdy:SkZd25etQDLEEBLB@cluster0.kbcoecs.mongodb.net/chatting-web?retryWrites=true&w=majority')
+                await mongoose.connect('mongodb+srv://AhmedMagdy:1YLcRgPR4L0fPQzW@cluster0.kbcoecs.mongodb.net/chatting-web?retryWrites=true&w=majority')
                 .then(()=>{
                         return cb()
                         .then( resalt =>{
